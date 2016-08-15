af2f6516668add7911498929d5dd72e3e6704e67
MagdalenaLysiak <magdalena.lysiak87@gmail.com>
2016-08-15 21:19:14 +0200

    First
    ...

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    var button = $("#go");
+    var avatar = $('.avatar');
+    var alert = $('#alert');
+
+    function sendAjax(password) {
+        if(!password.length) {
+            $('#alert').show().delay(2000).fadeOut(800);
+            $('#alertTriangle').show().delay(2000).fadeOut(800);
+        } else if(password.length<4){
+            $('#alertTwo').show().delay(2000).fadeOut(800);
+            $('#alertTriangle').show().delay(2000).fadeOut(800);
+    } else {
+        $.ajax({
+            type: "post",
+            data: {
+                login: "efi",
+                password: password,
+            },
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                console.log(response.responseText);
+            },
+            success: function(response) {
+                console.log(response);
+            }
+        });
+        window.location.href = "http://www.google.com";
+    };
+    };
+
+    button.on('click', function(event) {
+        event.preventDefault();
+        var info = $('.pass').val();
+        sendAjax(info);
+    });
+
+    avatar.on('click', function(event) {
+        alert(avatar.css('background-image'));
+
+    });
+
+});
