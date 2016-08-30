a8581e71ee8df109ffd36eb73e8021771d6e6874
MagdalenaLysiak <magdalena.lysiak87@gmail.com>
2016-08-30 20:02:48 +0200

    components
    ...

--- js/siteapp.js
+++ js/siteapp.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     
 var search = $('.search');
-var input = $('input');
+var input = $('.searchInput');
     
     search.on('mouseenter', function(){
         input.show("slow");
@@ -11,8 +11,29 @@ var input = $('input');
         input.hide("slow");
     })
 
-  
+  function getData(endpoint, callback) {
+        $.ajax({
+              type: 'GET',
+              url: "https://efigence-camp.herokuapp.com/api/"+endpoint,
+              dataType: 'json'
+        }).done(function(answer){
+            
+            callback(answer);
+             
+    	 }).fail(function(error) {
+           console.log("Błąd!")
+       })  
+}
+
+    getData( "data/summary" , function(data) {
+        $('.balanceNo').prepend("<b>" + data.content[0].balance + " " + "</b>");
+        $('.fundsNo').prepend("<b>" + data.content[0].funds  + " " + "</b>");   
+        $('.paymentsNo').prepend("<b>" + data.content[0].payments  + " " + "</b>");      
+        
+        
+    });
+
           
-};
+
     
 });
